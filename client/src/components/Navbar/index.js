85a66f60bb1d61f9e8a227a9c20da6c30d5c635d
Sean Duiser <duzer84@gmail.com>
2022-05-05 18:22:58 -0400

    added style to logout and user's profile button (when logged in)

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -21,10 +21,10 @@ function Navbar() {
   if (Auth.loggedIn()) {
     return (
       <div style={styles.navbar}>
-        <Link class="nav-btn btn-1" to="/me">
+        <Link style={{ backgroundColor: '#992154', padding: '10px', borderRadius: '15px', transform: 'rotate(15deg)', marginBottom: '20px'}} class="btn-border" to="/me">
           {Auth.getProfile().data.username}'s profile
         </Link>
-        <button onClick={logout}>
+        <button class="btn-border" style={{ transform: 'rotate(15deg)', backgroundColor: '#992154', padding: '10px', borderRadius: '15px', marginBottom: '20px'}} onClick={logout}>
           Logout
         </button>
       </div>
