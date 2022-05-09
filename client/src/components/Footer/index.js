a6e16668c033344da21ff84989c2b3bc24aad2d3
Baron Skidmore <baronhockeyplayer@gmail.com>
2022-05-08 20:13:15 -0400

    added footer and footer stylings

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,8 +2,33 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>
-      {/* Footer details HERE */}
+    <footer class="footer-distributed">
+
+      <div class="footer-left">
+
+        <h3>Pixel<span>Mixup</span></h3>
+
+        <p class="footer-link">
+          <a href="/" class="link-1">Home</a>
+        </p>
+      </div>
+
+      <div class="footer-center">
+
+        <div>
+          <p><span>444 S. Cedros Ave</span> Solana Beach, California</p>
+        </div>
+      </div>
+
+      <div class="footer-right">
+
+        <p class="footer-company-about">
+          <span>About the company</span>
+          Pixel Mixup allows you to create and save your creative pixel drawings!
+        </p>
+
+      </div>
+
     </footer>
   );
 };
